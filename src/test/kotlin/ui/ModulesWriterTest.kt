f0f1be720f708c94695054a3b161a176695e6525
Boris Farber <boris.farber@gmail.com>
2017-10-31 07:03:55 -0700

    updated image + test fixes

--- src/test/kotlin/ui/ModulesWriterTest.kt
+++ src/test/kotlin/ui/ModulesWriterTest.kt
@@ -4,15 +4,19 @@ import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Test
 import ui.generators.BuildGradleGenerator
+import ui.generators.project.GradleSettingsGenerator
 import ui.test_utils.mock
 
 class ModulesWriterTest {
 
     val fileWriter: FileWriter = mock()
+    val gradleSettingsGenerator: GradleSettingsGenerator = mock()
     val dependencyValidator: DependencyValidator = mock()
     val moduleBlueprinFactory: ModuleBlueprintFactory = mock()
     val buildGradleGenerator: BuildGradleGenerator = mock()
-    val modulesWriter = ModulesWriter(dependencyValidator, moduleBlueprinFactory, buildGradleGenerator, fileWriter)
+    val modulesWriter = ModulesWriter(dependencyValidator,
+            moduleBlueprinFactory,
+            buildGradleGenerator, gradleSettingsGenerator,fileWriter)
 
     @Test(expected = IllegalStateException::class)
     fun `generate throws ISE when input is invalid`() {
