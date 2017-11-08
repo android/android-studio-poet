9bb45b1105d5c91cb4bb0bc54de9c4d3bcea54bf
Boris Farber <boris.farber@gmail.com>
2017-11-08 22:41:31 +0200

    started activity generation

--- src/main/kotlin/main/generators/android_modules/ActivityGenerator.kt
+++ src/main/kotlin/main/generators/android_modules/ActivityGenerator.kt
@@ -1,7 +1,11 @@
 package main.generators.android_modules
 
+import com.squareup.javapoet.JavaFile
+import com.squareup.javapoet.TypeSpec
 import main.GenerationResult
 import main.models.AndroidModuleBlueprint
+import java.io.File
+import javax.lang.model.element.Modifier
 
 class ActivityGenerator {
 
@@ -12,6 +16,45 @@ class ActivityGenerator {
         return ActivityGenerationResult(0.until(blueprint.numOfActivities).map { "Activity$it" })
     }
 
+    fun generateClass(activityNumber: Int) {
+
+        val className = "Activity" + activityNumber
+
+        //TODO move to java poet
+        var classText = "package com.example.android.helloactivity;\n" +
+                "\n" +
+                "import android.app.Activity;\n" +
+                "import android.os.Bundle;\n" +
+                "\n" +
+                "\n" +
+                "public class HelloActivity extends Activity {\n" +
+                "    public HelloActivity() {\n" +
+                "    }\n" +
+                "\n" +
+                "    /**\n" +
+                "     * Called with the activity is first created.\n" +
+                "     */\n" +
+                "    @Override\n" +
+                "    public void onCreate(Bundle savedInstanceState) {\n" +
+                "        super.onCreate(savedInstanceState);\n" +
+                "\n" +
+                "        // Set the layout for this activity.  You can find it\n" +
+                "        // in res/layout/hello_activity.xml\n" +
+                "        setContentView(R.layout.hello_activity);\n" +
+                "    }\n" +
+                "}\n"
+
+
+
+
+
+
+    }
+
+
+
 }
 
-data class ActivityGenerationResult(val activityNames: List<String>): GenerationResult
+    data class ActivityGenerationResult(val activityNames: List<String>): GenerationResult
+}
+
