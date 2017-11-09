25ccd9af3b855993618c80d9bc7e6f126b306d05
Nikita Kozlov <nikita.e.kozlov@gmail.com>
2017-11-09 21:16:28 +0100

    Added android modules to the generated 'settings.gradle'

--- src/main/kotlin/main/generators/project/GradleSettingsGenerator.kt
+++ src/main/kotlin/main/generators/project/GradleSettingsGenerator.kt
@@ -1,5 +1,6 @@
 package main.generators.project
 
+import main.models.AndroidModuleBlueprint
 import main.writers.FileWriter
 import main.models.ModuleBlueprint
 import main.utils.joinPath
@@ -7,17 +8,17 @@ import main.utils.joinPath
 private const val INCLUDE_LENGTH_LIMIT = 250
 class GradleSettingsGenerator(private val fileWriter: FileWriter) {
 
-    fun generate(projectName: String, moduleBlueprints: List<ModuleBlueprint>, projectRoot: String) {
+    fun generate(projectName: String, moduleBlueprints: List<ModuleBlueprint>, androidModuleBlueprints: List<AndroidModuleBlueprint>, projectRoot: String) {
         val buff = StringBuilder()
         buff.append("rootProject.name = \'$projectName\'\n")
-        buff.append(generateIncludeStatements(moduleBlueprints))
+        buff.append(generateIncludeStatements(moduleBlueprints.map { it.name }))
+        buff.append(generateIncludeStatements(androidModuleBlueprints.map { it.name }))
 
         fileWriter.writeToFile(buff.toString(), projectRoot.joinPath("settings.gradle"))
     }
 
-    private fun generateIncludeStatements(moduleBlueprints: List<ModuleBlueprint>) =
+    private fun generateIncludeStatements(moduleBlueprints: List<String>) =
             moduleBlueprints.asSequence()
-                    .map { it.name }
                     .map { "\'$it\'" }
                     .foldIndexed("", { index, acc, next ->
                         acc + getIncludeStatementOrComma(index) + next
