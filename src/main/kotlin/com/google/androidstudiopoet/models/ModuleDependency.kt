7a837e658cd8981a6d45e446b664a041c45ac774
Boris Farber <boris.farber@gmail.com>
2017-11-21 14:36:00 +0200

    Fixed build.gradle dependencies naming

--- src/main/kotlin/com/google/androidstudiopoet/models/ModuleDependency.kt
+++ src/main/kotlin/com/google/androidstudiopoet/models/ModuleDependency.kt
@@ -1,3 +1,7 @@
 package com.google.androidstudiopoet.models
 
-data class ModuleDependency(val name: String, val methodToCall: MethodToCall)
+data class ModuleDependency(val name: String, val methodToCall: MethodToCall) {
+
+    override fun toString():String = name
+}
+
