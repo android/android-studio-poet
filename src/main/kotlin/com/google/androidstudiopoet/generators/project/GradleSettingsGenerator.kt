22fac9f25fc6499fb07ef43e164ce918e76035b5
Boris Farber <boris.farber@gmail.com>
2017-12-03 15:48:50 +0200

    verified licenses for all src files

--- src/main/kotlin/com/google/androidstudiopoet/generators/project/GradleSettingsGenerator.kt
+++ src/main/kotlin/com/google/androidstudiopoet/generators/project/GradleSettingsGenerator.kt
@@ -1,9 +1,23 @@
+/*
+Copyright 2017 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package com.google.androidstudiopoet.generators.project
 
-import com.google.androidstudiopoet.models.AndroidModuleBlueprint
-import com.google.androidstudiopoet.writers.FileWriter
-import com.google.androidstudiopoet.models.ModuleBlueprint
 import com.google.androidstudiopoet.utils.joinPath
+import com.google.androidstudiopoet.writers.FileWriter
 
 private const val INCLUDE_LENGTH_LIMIT = 250
 class GradleSettingsGenerator(private val fileWriter: FileWriter) {
