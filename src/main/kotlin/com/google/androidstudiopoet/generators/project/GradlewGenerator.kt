7657ae4281cddabc32624e3a89217c50a2f7c999
Boris Farber <boris.farber@gmail.com>
2017-12-18 15:57:08 +0200

    implementation

--- src/main/kotlin/com/google/androidstudiopoet/generators/project/GradlewGenerator.kt
+++ src/main/kotlin/com/google/androidstudiopoet/generators/project/GradlewGenerator.kt
@@ -18,7 +18,7 @@ package com.google.androidstudiopoet.generators.project
 
 import com.google.androidstudiopoet.models.ProjectBlueprint
 import com.google.androidstudiopoet.utils.joinPath
-import com.google.androidstudiopoet.writers.RemoteFilesWriter
+import com.google.androidstudiopoet.writers.GithubDownloader
 import java.io.File
 import java.io.FileInputStream
 import java.nio.file.Files
@@ -31,9 +31,21 @@ object GradlewGenerator {
         val gradlew = "gradlew"
         val gradlewbat = "gradlew.bat"
 
+        if (!File(getAssetsFolderPath()).exists() ||
+                !File(getAssetsFolderPath(), gradlew).exists() ||
+                !File(getAssetsFolderPath(), gradlewbat).exists() ||
+                !File(getAssetsFolderPath(), "gradle/wrapper").exists()
+                ) {
+
+            println("AS Poet needs network access to download gradle files from Github " +
+                    "\nhttps://github.com/android/android-studio-poet/tree/master/resources/gradle-assets " +
+                    "\nplease copy/paste the gradle folder directly to the generated root folder")
+            return
+        }
+
         val gradleWFile = File(root, gradlew).toPath()
-        Files.copy(FileInputStream(File(getAssetsPath(), gradlew)), gradleWFile, StandardCopyOption.REPLACE_EXISTING)
-        Files.copy(FileInputStream(File(getAssetsPath(), gradlewbat)), File(root, gradlewbat).toPath(), StandardCopyOption.REPLACE_EXISTING)
+        Files.copy(FileInputStream(File(getAssetsFolderPath(), gradlew)), gradleWFile, StandardCopyOption.REPLACE_EXISTING)
+        Files.copy(FileInputStream(File(getAssetsFolderPath(), gradlewbat)), File(root, gradlewbat).toPath(), StandardCopyOption.REPLACE_EXISTING)
 
         Runtime.getRuntime().exec("chmod u+x " + gradleWFile)
 
@@ -42,30 +54,36 @@ object GradlewGenerator {
         val gradleWrapperFolder = File(gradleFolder, "wrapper")
         gradleWrapperFolder.mkdir()
 
-        val sourceFolder = File(getAssetsPath(), "gradle/wrapper")
+        val sourceFolder = File(getAssetsFolderPath(), "gradle/wrapper")
 
         val gradleWrapperJar = "gradle-wrapper.jar"
         val gradleWrapperProperties = "gradle-wrapper.properties"
 
         Files.copy(FileInputStream(File(sourceFolder, gradleWrapperJar)),
-                File(gradleWrapperFolder, gradleWrapperJar).toPath(), StandardCopyOption.REPLACE_EXISTING)
+                File(gradleWrapperFolder, gradleWrapperJar).toPath(),
+                StandardCopyOption.REPLACE_EXISTING)
 
-        File(gradleWrapperFolder, gradleWrapperProperties).writeText(gradleWrapper(projectBlueprint.gradleVersion))
+        File(gradleWrapperFolder, gradleWrapperProperties).
+                writeText(gradleWrapper(projectBlueprint.gradleVersion))
     }
 
-    private fun getAssetsPath(): String {
+    private fun getAssetsFolderPath(): String {
 
-        var assetsFolder: String = System.getProperty("user.home").joinPath(".aspoet").joinPath("assets")
+        var assetsFolder: String = System.getProperty("user.home")
+                .joinPath(".aspoet")
+                .joinPath("gradle-assets")
 
-        if(!File(assetsFolder).exists()) {
-            println("not here yet")
-            var result: String = RemoteFilesWriter("", assetsFolder).getResultPath()
+        if (!File(assetsFolder).exists()) {
+            GithubDownloader().downloadDir(
+                    "https://api.github.com/repos/android/" +
+                            "android-studio-poet/contents/resources/gradle-assets?ref=master",
+                    assetsFolder)
         }
 
-        return "src/main/assets"
+        return assetsFolder
     }
 
-    fun gradleWrapper(gradleVersion: String) = """
+    private fun gradleWrapper(gradleVersion: String) = """
 distributionBase=GRADLE_USER_HOME
 distributionPath=wrapper/dists
 zipStoreBase=GRADLE_USER_HOME
