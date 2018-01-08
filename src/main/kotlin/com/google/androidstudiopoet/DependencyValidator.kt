00b0fd490270b11fe010aae1346c7daec498de20
Sergio Murguia <srmurguia@google.com>
2018-01-08 11:23:30 -0800

    Add feedback from Nikita on DependencyValidator.kt:
    - Make mdoule types constants
    - Move ModuleSplit to end of file
    - Create a couple of functions that check some dependency rules instead
    of having the checks directly on trhe function that uses them.
    
    All those changes made the code cleaner.

--- src/main/kotlin/com/google/androidstudiopoet/DependencyValidator.kt
+++ src/main/kotlin/com/google/androidstudiopoet/DependencyValidator.kt
@@ -18,11 +18,10 @@ package com.google.androidstudiopoet
 
 import com.google.androidstudiopoet.models.DependencyConfig
 
+private const val ANDROID_TYPE: String = "androidAppModule"
+private const val MODULE_TYPE = "module"
+
 class DependencyValidator {
-
-    private val ANDROID_TYPE = "androidAppModule"
-    private val MODULE_TYPE = "module"
-
     fun isValid(dependencies: List<DependencyConfig>, moduleCount: Int, androidModuleCount: Int): Boolean {
         for (dependency in dependencies) {
             if (badDependency(dependency, moduleCount, androidModuleCount)) {
@@ -32,6 +31,38 @@ class DependencyValidator {
         //TODO Add check for cycle dependencies and proper reporting
         return true
     }
+
+    private fun badDependency(dependency: DependencyConfig, moduleCount: Int, androidModuleCount: Int): Boolean {
+        val from = ModuleSplit(dependency.from)
+        if (badIndexByType(from, moduleCount, androidModuleCount)) {
+            return true
+        }
+        val to = ModuleSplit(dependency.to)
+        if (badIndexByType(to, moduleCount, androidModuleCount)) {
+            return true
+        }
+        if (javaDependsOnAndroid(from, to)) {
+            return true
+        }
+        if (isApp(to)) {
+            return true
+        }
+        return false
+    }
+
+    private fun badIndexByType(moduleSplit: ModuleSplit, moduleCount: Int, androidModuleCount: Int): Boolean =
+            when(moduleSplit.type) {
+                MODULE_TYPE -> badIndex(moduleSplit.index, moduleCount)
+                ANDROID_TYPE -> badIndex(moduleSplit.index, androidModuleCount)
+                else -> true
+            }
+
+    private fun badIndex(index: Int, count: Int) = (index < 0) || (index >= count)
+
+    private fun javaDependsOnAndroid(from: ModuleSplit, to: ModuleSplit): Boolean =
+            from.type.equals(MODULE_TYPE) && to.type.equals(ANDROID_TYPE)
+
+    private fun isApp(to: ModuleSplit): Boolean = to.index == 0 && to.type.equals(ANDROID_TYPE)
 
     private class ModuleSplit(moduleName : String){
         val type: String
@@ -46,37 +77,4 @@ class DependencyValidator {
             index = moduleName.substring(firstChar + 1).toInt()
         }
     }
-
-    private fun badDependency(dependency: DependencyConfig, moduleCount: Int, androidModuleCount: Int): Boolean {
-        val from = ModuleSplit(dependency.from)
-        // Check types and indexes
-        if (badIndexByType(from, moduleCount, androidModuleCount)) {
-            return true
-        }
-        val to = ModuleSplit(dependency.to)
-        if (badIndexByType(to, moduleCount, androidModuleCount)) {
-            return true
-        }
-        // check that a java module does not depend on an Android module
-        if (from.type.equals(MODULE_TYPE) && to.type.equals(ANDROID_TYPE)) {
-            return true
-        }
-        // Check that an Android module does not depend on an app
-        if (from.type.equals(ANDROID_TYPE) && to.type.equals(ANDROID_TYPE) && (to.index == 0)) {
-            return true
-        }
-        return false
-    }
-
-    private fun badIndexByType(moduleSplit: ModuleSplit, moduleCount: Int, androidModuleCount: Int): Boolean {
-        if (moduleSplit.type.equals(MODULE_TYPE)) {
-            return badIndex(moduleSplit.index, moduleCount)
-        }
-        if (moduleSplit.type.equals(ANDROID_TYPE)) {
-            return badIndex(moduleSplit.index, androidModuleCount)
-        }
-        return true
-    }
-
-    private fun badIndex(index: Int, count: Int) = (index < 0) || (index >= count)
 }
