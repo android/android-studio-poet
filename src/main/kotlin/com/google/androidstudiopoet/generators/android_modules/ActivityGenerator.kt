1580d344f99692cdec7af6ee6c11f9ee73fa74b9
Nikita Kozlov <nikita.e.kozlov@gmail.com>
2018-02-08 22:55:44 +0100

    Refactored `ActivityGenerator` to use JavaPoet and to call method Added a method call to the class from blueprint

--- src/main/kotlin/com/google/androidstudiopoet/generators/android_modules/ActivityGenerator.kt
+++ src/main/kotlin/com/google/androidstudiopoet/generators/android_modules/ActivityGenerator.kt
@@ -17,37 +17,52 @@ limitations under the License.
 package com.google.androidstudiopoet.generators.android_modules
 
 import com.google.androidstudiopoet.models.ActivityBlueprint
+import com.google.androidstudiopoet.models.ClassBlueprint
 import com.google.androidstudiopoet.writers.FileWriter
+import com.squareup.javapoet.*
+import javax.lang.model.element.Modifier
+
 
 class ActivityGenerator(var fileWriter: FileWriter) {
 
     fun generate(blueprint: ActivityBlueprint) {
-        val classText =
-                "package ${blueprint.packageName};\n" +
-                        "import android.app.Activity;\n" +
-                        "import android.os.Bundle;\n" +
-                        "import ${blueprint.packageName}.R;\n" +
-                        "\n" +
-                        "\n" +
-                        "public class " + blueprint.className + " extends Activity {\n" +
-                        "    public ${blueprint.className}() {\n" +
-                        "    }\n" +
-                        "\n" +
-                        "    /**\n" +
-                        "     * Called with the activity is first created.\n" +
-                        "     */\n" +
-                        "    @Override\n" +
-                        "    public void onCreate(Bundle savedInstanceState) {\n" +
-                        "        super.onCreate(savedInstanceState);\n" +
-                        "\n" +
-                        "        // Set the layout for this activity.  You can find it\n" +
-                        "        // in res/layout/hello_activity.xml\n" +
-                        "        setContentView(R.layout." + blueprint.layout + ");\n" +
-                        "    }\n" +
-                        "}\n"
-        println("${blueprint.where}/${blueprint.className}.java")
-        fileWriter.writeToFile(classText, "${blueprint.where}/${blueprint.className}.java")
 
+        val classBlueprint = blueprint.classBlueprint
+        val onCreateMethodStatements = getMethodStatementFromClassToCall(classBlueprint)?.let { listOf(it) } ?: listOf()
+
+        val onCreateMethod = getOnCreateMethod(blueprint.layout, onCreateMethodStatements)
+
+        val activityClass = getClazzSpec(blueprint.className)
+                .addMethod(onCreateMethod)
+                .build()
+
+        val javaFile = JavaFile.builder(blueprint.packageName, activityClass).build()
+
+        println("${blueprint.where}/${blueprint.className}.java")
+        fileWriter.writeToFile(javaFile.toString(), "${blueprint.where}/${blueprint.className}.java")
+
+    }
+
+    private fun getMethodStatementFromClassToCall(classBlueprint: ClassBlueprint) =
+            classBlueprint.getMethodToCallFromOutside()?.let { "new ${it.className}().${it.methodName}()" }
+
+    private fun getClazzSpec(activityClassName: String) =
+            TypeSpec.classBuilder(activityClassName)
+                    .superclass(TypeVariableName.get("android.app.Activity"))
+                    .addModifiers(Modifier.PUBLIC)
+
+    private fun getOnCreateMethod(layout: String, statements: List<String>): MethodSpec {
+
+        val builder = MethodSpec.methodBuilder("onCreate")
+                .addAnnotation(Override::class.java)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(Void.TYPE)
+                .addParameter(TypeVariableName.get("android.os.Bundle"), "savedInstanceState")
+                .addStatement("super.onCreate(savedInstanceState)")
+                .addStatement("setContentView(R.layout.$layout)")
+
+        statements.forEach { builder.addStatement(it) }
+        return builder.build();
     }
 
 }
