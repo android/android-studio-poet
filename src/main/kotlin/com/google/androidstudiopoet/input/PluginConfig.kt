942af6d4135384e501098f0ffee902d26521c732
Nikita Kozlov <nikita.e.kozlov@gmail.com>
2018-05-26 08:51:17 +0200

    Added Javadoc to the `PluginConfig`

--- src/main/kotlin/com/google/androidstudiopoet/input/PluginConfig.kt
+++ src/main/kotlin/com/google/androidstudiopoet/input/PluginConfig.kt
@@ -1,3 +1,10 @@
 package com.google.androidstudiopoet.input
 
+/**
+ * Configuration of a plugin applied to a module.
+ *
+ * @param id Id of the plugin that will be used in the gradle script to apply it
+ * @param taskName Name of the gradle task that is used to customize plugin
+ * @param taskBody Body of the gradle task that is used to customize plugin
+ */
 data class PluginConfig(val id: String, val taskName: String? = null, val taskBody: List<String>? = null)
