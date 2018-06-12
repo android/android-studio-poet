336feb1860d1973a2eee3bdaf548a48dd89c3036
Nikita Kozlov <nikita.e.kozlov@gmail.com>
2018-06-12 22:44:07 +0200

    Fixed test build

--- src/test/kotlin/com/google/androidstudiopoet/models/AndroidModuleBlueprintTest.kt
+++ src/test/kotlin/com/google/androidstudiopoet/models/AndroidModuleBlueprintTest.kt
@@ -24,11 +24,11 @@ class AndroidModuleBlueprintTest {
         assertOn(androidModuleBlueprint) {
             activityBlueprints.assertEquals(listOf(ActivityBlueprint(
                     "Activity0",
-                    androidModuleBlueprint.resourcesBlueprint!!.layoutNames[0],
+                    androidModuleBlueprint.resourcesBlueprint!!.layoutBlueprints[0],
                     androidModuleBlueprint.packagePath,
                     androidModuleBlueprint.packageName,
                     androidModuleBlueprint.packagesBlueprint.javaPackageBlueprints[0].classBlueprints[0],
-                    listOf())))
+                    listOf(), false)))
         }
     }
 
